b29572474979fc68c847350623c05958a8b318fe
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2017-03-08 17:49:51 +0100

    added missing symbols

--- playground/student_project/tasks solutions/task3.cpp
+++ playground/student_project/tasks solutions/task3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vpipes.hpp>
-#include "tasks.hpp"
+#include <random>
 
 using namespace std;
 using namespace mondrian::vpipes;
@@ -13,6 +13,31 @@ using namespace mondrian::vpipes;
             (*result)[i++] = *it;                                                       \
     }                                                                                   \
     *result_size = i;                                                                   \
+}
+
+using namespace std;
+
+default_random_engine generator;
+
+int random_number()
+{
+    static uniform_int_distribution<int> distribution(numeric_limits<int>::min(), numeric_limits<int>::max());
+    return distribution(generator);
+};
+
+int *create_column(unsigned long num_of_elements, bool fill_with_random = true, bool fill = true)
+{
+    auto result = (int *) malloc (num_of_elements * sizeof(int));
+    if (fill) {
+        for (auto i = 0; i < num_of_elements; ++i)
+            result[i] = fill_with_random ? random_number() : i;
+    }
+    return result;
+}
+
+void delete_column(int *column)
+{
+    free (column);
 }
 
 int main() {
