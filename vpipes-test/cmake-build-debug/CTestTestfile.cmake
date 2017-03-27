07b80d309b6c7be097c0a7712d120a9847b4c46b
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2017-03-27 09:28:05 +0200

    moved "vpipes_testing" directory one level up, such that it is in the root of MondrianDB (as "vpipes-testing")

--- vpipes-test/cmake-build-debug/CTestTestfile.cmake
+++ vpipes-test/cmake-build-debug/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /Users/marcus/git/MondrianDB/mondrian-tests
-# Build directory: /Users/marcus/git/MondrianDB/mondrian-tests/cmake-build-debug
+# Source directory: /Users/marcus/git/MondrianDB/vpipes-test
+# Build directory: /Users/marcus/git/MondrianDB/vpipes-test/cmake-build-debug
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
