fae534e60ee210c193d622be8b48ca3126b90858
Pegasus99 <mmohsen35@gmail.com>
2017-05-04 16:19:00 +0200

    added implementation for: 1-branchfree preicate based on the straightforward implementation. 2- micro_branchfree based on the micro optimized implementation. 3- unrolled predicate based on the micro optimized implementation. 4- tests for both (micro_branchfree & unrolled predicate) the tests are based on the tests for the straightforward predicate tests. also added #include<bitset> to smart_bitmask.hpp. modified in attribute_switch.hpp (point_null_copy) to (m_point_null_copy)

--- vpipes-test/cmake-build-debug/CTestTestfile.cmake
+++ vpipes-test/cmake-build-debug/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /Users/marcus/git/MondrianDB/vpipes-test
-# Build directory: /Users/marcus/git/MondrianDB/vpipes-test/cmake-build-debug
+# Source directory: /home/pegasus/Dokumente/work/codes/develop_branch_free_null_supported/MondrianDB/vpipes-test
+# Build directory: /home/pegasus/Dokumente/work/codes/develop_branch_free_null_supported/MondrianDB/vpipes-test/cmake-build-debug
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
